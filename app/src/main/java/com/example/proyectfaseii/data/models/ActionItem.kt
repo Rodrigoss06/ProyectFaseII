ea84f83b09b4b522835c3bbea7c8f87dc9103314
Rodrigoss06 <rodrigo.ss0606@gmail.com>
2025-06-27 09:57:17 -0500

    feat: Integrar modelo de Datos

---
+++ app/src/main/java/com/example/proyectfaseii/data/models/ActionItem.kt
@@ -0,0 +1,11 @@
+// ActionItem.kt
+package com.example.proyectfaseii.data.models
+
+data class ActionItem(
+    val id: String = "",
+    val remind_at: String = "",
+    val status: Int = 0,
+    val title: String = "",
+    val updated_at: String = "",
+    val habit_id: String = ""
+)
---
+++ app/src/main/java/com/example/proyectfaseii/data/models/Goal.kt
@@ -0,0 +1,8 @@
+// Goal.kt
+package com.example.proyectfaseii.data.models
+
+data class Goal(
+    val unit_type: String = "",
+    val value: Double = 0.0,
+    val periodicity: String = ""
+)
